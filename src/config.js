585475e3d870b969cc31641066d214b116a8d9ce
anas1412 <anas.bassoumi@gmail.com>
2024-12-21 23:43:27 +0100

    added an endpoint for backend for vercel

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
+//const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://geminiwire.onrender.com";
 
 export default API_BASE_URL;
