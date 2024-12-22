9e655188e5669d5707709d6dabbb2f9b8c99c627
anas1412 <anas.bassoumi@gmail.com>
2024-12-22 18:32:49 +0100

    Fixed the updated function

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
-//const API_BASE_URL = "https://geminiwire.onrender.com";
+//const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://geminiwire.onrender.com";
 
 export default API_BASE_URL;
