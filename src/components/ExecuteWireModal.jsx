59f32f4ac9943527e7844de3976d8564049729e8
anas1412 <anas.bassoumi@gmail.com>
2025-01-02 00:34:12 +0100

    added a modal for wires

--- src/components/ExecuteWireModal.jsx
+++ src/components/ExecuteWireModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 const ExecuteWireModal = ({ wire, onExecute, onClose }) => {
   const [inputs, setInputs] = useState({});
@@ -28,7 +29,7 @@ const ExecuteWireModal = ({ wire, onExecute, onClose }) => {
 
   return (
     <>
-      <h2 className="mb-4 text-2xl font-bold">Execute Wire: {wire.wire_id}</h2>
+      <h2 className="mb-4 text-2xl font-bold">Execute Wire: {wire?.wire_id}</h2>
       {isLoading && <LoadingSpinner />}
       {error && (
         <div className="px-4 py-3 mb-4 text-red-700 bg-red-100 border border-red-400 rounded">
@@ -58,21 +59,22 @@ const ExecuteWireModal = ({ wire, onExecute, onClose }) => {
         </div>
       ) : (
         <form onSubmit={handleSubmit}>
-          {Object.entries(wire.inputs).map(([key, description]) => (
-            <div key={key} className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                {key}
-              </label>
-              <input
-                type="text"
-                value={inputs[key] || ""}
-                onChange={(e) => handleInputChange(key, e.target.value)}
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm"
-                placeholder={description}
-                required
-              />
-            </div>
-          ))}
+          {wire?.inputs &&
+            Object.entries(wire.inputs).map(([key, description]) => (
+              <div key={key} className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  {key}
+                </label>
+                <input
+                  type="text"
+                  value={inputs[key] || ""}
+                  onChange={(e) => handleInputChange(key, e.target.value)}
+                  className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm"
+                  placeholder={description}
+                  required
+                />
+              </div>
+            ))}
           <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
